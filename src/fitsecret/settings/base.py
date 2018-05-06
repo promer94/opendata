ef4e93f6bc99ece84a6b301130378478f0f00dfb
Yixuan Xu <yixuanxu94@gmail.com>
2018-05-06 14:54:45 +0100

    :rocket: First depolyment

--- src/fitsecret/settings/base.py
+++ src/fitsecret/settings/base.py
@@ -128,7 +128,6 @@ LOGIN_REDIRECT_URL = '/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
