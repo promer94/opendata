30488ffde2cb7d7170d4dd335df2d8178d7f9cc4
Yixuan Xu <yixuanxu94@gmail.com>
2018-05-06 03:45:46 +0100

    :rocket: First Deploy

--- src/fitsecret/settings/production.py
+++ src/fitsecret/settings/production.py
@@ -128,8 +128,8 @@ LOGIN_REDIRECT_URL = '/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "apperance"),
 ]
